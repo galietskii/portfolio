31494699d09bdb062137e7f1906d21e336a2ae41
Ilia <63251213+galietskii@users.noreply.github.com>
2021-03-20 18:03:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ open.addEventListener("click",      function() {
     else popup.style.display = 'none';
     
   });
+// ACARDION MODULES
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
