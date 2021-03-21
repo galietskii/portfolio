005c06f16c251ed3a635e15323d26f03adbe37a4
Ilia <63251213+galietskii@users.noreply.github.com>
2021-03-21 21:26:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-let popup = document.querySelector('.page-navigation__list');
-let open = document.querySelector('.button-burger');
-popup.style.display = 'none';
-popup.style.height = '0';
+// let popup = document.querySelector('.page-navigation__list');
+// let open = document.querySelector('.button-burger');
+// popup.style.display = 'none';
+// popup.style.height = '0';
 
-open.addEventListener("click",      function() {
-	open.style.display = 'none';
-    if(popup.style.display == 'none')  
-    popup.style.display = 'block';
-    else popup.style.display = 'none';
+// open.addEventListener("click",      function() {
+// 	open.style.display = 'none';
+//     if(popup.style.display == 'none')  
+//     popup.style.display = 'block';
+//     else popup.style.display = 'none';
     
+//   });
+let overlay = document.querySelector('.burger_overlay')
+let coll = document.getElementsByClassName('button-burger');
+for(let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+      this.classList.toggle('actives');
+      let content = this.nextElementSibling;
+      if(content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + 'px';
+      }
   });
+}
 // ACARDION MODULES
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
